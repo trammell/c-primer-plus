fa23ba3b9a834f9d3761024262519a82320dee64
John Trammell <john.trammell@gmail.com>
2019-06-23 17:00:40 -0500

    added simple

---
+++ chapter-02/simple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() /* a simple program */
+{
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorite number number is %d because it is first.\n", num);
+}
