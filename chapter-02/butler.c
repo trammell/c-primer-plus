6d125399322066d99511e2049d8447191aba0115
John Trammell <john.trammell@gmail.com>
2019-07-03 23:34:28 -0500

    done with chapter 2

---
+++ chapter-02/butler.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void butler(void) {
+    printf("You rang, sir?\n");
+}
+
+void main(void) {
+    printf("I will summon the butler function.\n");
+    butler();
+    printf("Yes. Bring me some tea.\n");
+}
+
---
+++ chapter-02/fathom.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void) {
+    int feet, fathoms;
+    fathoms = 2;
+    feet = 6 * fathoms;
+    printf("There are %d feet in %d fathoms.\n", feet, fathoms);
+}
