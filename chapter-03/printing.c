dd9b5c5b4b88550c3f42acd9064d6249e0a1ebd1
John Trammell <john.trammell@gmail.com>
2019-07-04 00:18:24 -0500

    fixed printing with correct limits

--- chapter-03/printing.c
+++ chapter-03/printing.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main() {
-    unsigned u = 40000;
-    long lo = 2000000000; /* nine zeroes */
-    unsigned long ulo = 2 * 2000000000;
-    printf("u = %u and not %d\n", u, u);
+    unsigned u = 4000000000;
+    long lo = 20000000000L;
+    unsigned long ulo = 2 * lo;
+    printf("  u = %u and not %d\n", u, u);
+    printf(" lo = %ld and not %d\n", lo, lo);
+    printf("ulo = %lu and not %d\n", ulo, ulo);
 }
